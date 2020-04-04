1843945680a42839a1d51664a0f6dcd6db514152
peter <peters.shaker@gmail.com>
2020-04-04 23:35:51 +0200

    formating master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Paper  from "./Paper";
 
 function App() {
-const arr = [10 , 20 , 40]
-const res = arr.find(item =>{
-  console.log(item > 15)
- return  item > 15
-} )
-console.log('res', res)
-
   return (
     <div className="App">
       <header className="App-header">
@@ -18,21 +10,6 @@ console.log('res', res)
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
-
-             {/* <Paper  test={'world'} /> */}
-        {/* {arr.map((item ,index)=>{
-      console.log(item)
-      console.log('index', index)
-          return (
-            <Paper key={index} str={item} test={'test'}/>
-
-        <Paper str={'item'}/> */}
-         {/* {arr.map((item ,index)=>{
-         return  <Paper  num={'str'} str={item} />
-         })} */}
-          <Paper  num={'str'} />
-
         <a
           className="App-link"
           href="https://reactjs.org"
